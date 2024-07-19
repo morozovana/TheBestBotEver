24263cb4108c9f46e9e5e25cf357de856627a274
nadya <you@example.com>
2024-07-19 14:24:31 +0700

    Put [httpServer] into package and make go routine in [main]

--- main.go
+++ main.go
@@ -56,6 +56,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to initialize db: %s", err.Error())
 	}
+
+	_ = db
+
 	//bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
